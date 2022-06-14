352908d3ed192bdfc9ab13b812c09facbd089129
Nutifafa Attor <attorfafa@gmail.com>
2022-06-14 14:08:35 +0000

    buttons

--- src/components/SwipeButtons/SwipeButtons.jsx
+++ src/components/SwipeButtons/SwipeButtons.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 import './SwipeButtons.css'
 
+import ReplayIcon from '@mui/icons-material/Replay';
+import CloseIcon from '@mui/icons-material/Close';
+import StarRateIcon from '@mui/icons-material/StarRate';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FlashOnIcon from '@mui/icons-material/FlashOn';
+import { IconButton } from '@mui/material'
+
+
 const SwipeButtons = () => {
   return (
-    <div>SwipeButtons</div>
+    <div className='swipeButtons'>
+      <IconButton className='swipeButtons__repeat'>
+        <ReplayIcon fontSize='large' />
+      </IconButton>
+      <IconButton className='swipeButtons__left'>
+        <CloseIcon fontSize='large' />
+      </IconButton>
+      <IconButton className='swipeButtons__star'>
+        <StarRateIcon fontSize='large' />
+      </IconButton>
+      <IconButton className='swipeButtons__right'>
+        <FavoriteIcon fontSize='large' />
+      </IconButton>
+      <IconButton className='swipeButtons__lightning'>
+        <FlashOnIcon fontSize='large' />
+      </IconButton>
+    </div>
   )
 }
 
