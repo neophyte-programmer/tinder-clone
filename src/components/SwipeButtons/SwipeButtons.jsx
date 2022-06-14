c7e888cc182015c9b8817e7c8345eb6d51a0dae8
Nutifafa Attor <attorfafa@gmail.com>
2022-06-14 11:22:32 +0000

    cleaning up file structure

---
+++ src/components/SwipeButtons/SwipeButtons.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import 'SwipeButtons.css'
+
+const SwipeButtons = () => {
+  return (
+    <div>SwipeButtons</div>
+  )
+}
+
+export default SwipeButtons
---
+++ src/components/TinderCards/TinderCards.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import 'TinderCards.css'
+
+const TinderCards = () => {
+  return (
+    <div>TinderCards</div>
+  )
+}
+
+export default TinderCards
