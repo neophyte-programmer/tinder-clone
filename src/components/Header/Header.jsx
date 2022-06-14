1bc40c00816dc74bbaafe3111ae36cfd45a22db2
Nutifafa Attor <attorfafa@gmail.com>
2022-06-14 12:13:45 +0000

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import './Header.css'
+import logo from '../../images/logo.png'
+
+import PersonIcon from '@mui/icons-material/Person'
+import ForumIcon from '@mui/icons-material/Forum';
+import { IconButton } from '@mui/material'
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
+	return (
+		<div className='header'>
+			<IconButton>
+				<PersonIcon fontSize='large' className='header__icon' />
+            </IconButton>
+            
+            <img src={logo} alt="tinder-logo" className='header__logo' />
+
+            <IconButton>
+                <ForumIcon fontSize='large' className='header__icon' />
+            </IconButton>
+		</div>
+	)
 }
 
 export default Header
