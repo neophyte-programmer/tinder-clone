a18f784e661dba235bdefa3432b5aa86de7770c7
Nutifafa Attor <attorfafa@gmail.com>
2022-06-14 11:58:29 +0000

    layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import Header from './components/Header/Header';
+import SwipeButtons from './components/SwipeButtons/SwipeButtons';
+import TinderCards from './components/TinderCards/TinderCards';
 
 function App() {
   return (
     <div className="app">
       {/* Header */}
+      <Header />
       {/* Tinder Cards */}
+      <TinderCards />
       {/* Swipe Buttons */}
+      <SwipeButtons />
     </div>
   );
 }
